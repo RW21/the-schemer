d1e8fac06b621d973e83ce902e50f1c9b887a8a0
RW21 <rinze.watanabe.translation@gmail.com>
2020-07-08 23:09:26 +1000

    Update

--- little_schemer.rkt
+++ little_schemer.rkt
@@ -19,3 +19,25 @@
     [(null? lat) #f]
     [(eq? a (car lat)) #t]
     [else (member_? a (cdr lat))]))
+
+(define (rember a lat)
+  (cond
+    [(null? lat) '()]
+    [(eq? a (car lat)) (cdr lat)]
+    [else (cons (car lat) (rember a (cdr lat)))]))
+
+(define (firsts lst)
+  (if
+   (null? lst)
+   ('())
+    ((cons (car (car lst)) (firsts (cdr lst))))))
+
+(define (firsts_ lst)
+  (map (lambda (a) (car a)) lst))
+
+(define (insertR new old lat)
+  (cond
+    [(null? lat) '()]
+    [(eq? (car lat) old) (cons old
+                              (cons new (cdr lat)))]
+    [else (cons (car lat) (insertR new old (cdr lat)))]))
