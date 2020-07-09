a9580b78b907b75abd3d6783401559ccda7a0a99
Rinze Watanabe <rinze.watanabe.translation@gmail.com>
2020-07-09 12:30:58 +1000

    Update

--- little_schemer.rkt
+++ little_schemer.rkt
@@ -41,3 +41,14 @@
     [(eq? (car lat) old) (cons old
                               (cons new (cdr lat)))]
     [else (cons (car lat) (insertR new old (cdr lat)))]))
+
+
+(define (insertL new old lat)
+  (cond
+    [(null? lat) '()]
+    [(eq? (car lat) old) (cons new lat)]
+    [else (cons (car lat) (insertR new old (cdr lat)))]))
+
+
+(define (subst new old lat)
+  (cond 
